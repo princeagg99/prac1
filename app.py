9247acc05a1ec1d7f2750c1fbb10cffbdeca14d1
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 14:55:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ def summarize_gpt():
     session_state = st.session_state
 
     if 's1' not in session_state:
-        session_state.s1 = False
+        session_state.s1 = "start"
     if 's2' not in session_state:
-        session_state.s2 = True
+        session_state.s2 = "start"
 
     st.markdown("""<span style="font-size: 24px; ">Summarize key findings of the case.</span>""", unsafe_allow_html=True)
     st.write() #This is to have gap between
     #summ_gpt = st.button("Summarize")
-    if session_state.s1 == False:
+    if session_state.s1 == "start":
         if st.button('Summarize'):
             with st.spinner("Summarizing...."):
         
@@ -19,25 +19,23 @@ def summarize_gpt():
     
                 st.markdown("#### Summarization Feedback:")
                 col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
+                session_state.s1 == "feed"
     
-                if session_state.s1 == False:
-                    with col_1:
-                        if st.button("👍🏻",key=4):
-                            
-                            session_state.s2 = False
-                             
-    
-    
-                    with col_2:
-                        if st.button("👎🏻",key=5):
-                            
-    
-    
-                            session_state.s2 = False
+    if session_state.s1 == "feed":
+        with col_1:
+            if st.button("👍🏻",key=4):
+                
+                session_state.s2 = "call"
+                 
+
+
+        with col_2:
+            if st.button("👎🏻",key=5):
+            session_state.s2 = "call"
                            
 
-                if session_state.s2 == False:
-                   st.write('Feedback is recorded!')
+    if session_state.s2 == "call":
+       st.write('Feedback is recorded!')
         
         
 summarize_gpt()
