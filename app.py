98b93987cde03bacbd0395c6e06ceb9a555484c6
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 13:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,37 +4,33 @@ def summarize_gpt():
 
     
     if 'summary_genenrated' not in st.session_state:
-        st.session_state.summary_genenrated = True
+        st.session_state.summary_genenrated = False
 
 
     st.markdown("""<span style="font-size: 24px; ">Summarize key findings of the case.</span>""", unsafe_allow_html=True)
     st.write() #This is to have gap between
     summ_gpt = st.button("Summarize-me")
-    if summ_gpt:
-        with st.spinner("Summarizing...."):
-            st.session_state.summary_genenrated = True
-            st.write("abcdefgh")
-            st.write("1234")
+    if st.session_state.summary_genenrated = False:
+        
+        if summ_gpt:
             
-            st.markdown("#### Summarization Feedback:")
-            col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
-
-            with col_1:
-                if st.button("👍🏻",key=4)
-                    st.write("*Feedback is recorded*")
-                    st.session_state.summary_genenrated = True
-
-
-            with col_2:
-                if st.button("👎🏻",key=5):
-                    
-
-
-                    st.write("*Feedback is recorded*")
-                    # st.session_state.summary_genenrated = True
+            with st.spinner("Summarizing...."):
+                st.session_state.summary_genenrated = True
+                st.write("abcdefgh")
+                st.write("1234")
+                
+                st.markdown("#### Summarization Feedback:")
+                col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
+    
+                with col_1:
+                        st.button("👍🏻",key=4)
+                         
+                        st.write("*Feedback is recorded*")
+                        st.session_state.summary_genenrated = True
+    
 
     elif st.session_state.summary_genenrated == True:
-            st.write("*Thank you, your feedback is recorded*") 
+        st.write("*Thank you, your feedback is recorded*") 
 
 
 
