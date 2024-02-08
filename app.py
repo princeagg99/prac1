afdc96e97937a95b57f38691fededb01b5d2d5e0
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 14:09:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
-import streamlit as st
+def summarize_gpt(tmp_table_gpt):
 
-# Create a session state object
-session_state = st.session_state
+    session_state = st.session_state
 
-# Initialize session state variables
-if 'button_clicked' not in session_state:
-    session_state.button_clicked = False
+    if 's1' not in session_state:
+        session_state.s1 = False
+    if 's2' not in session_state:
+        session_state.s2 = False
 
-# Create the first button
-if st.button('summarize'):
-    st.write('Button 1 was ## clicked')
+    st.markdown("""<span style="font-size: 24px; ">Summarize key findings of the case.</span>""", unsafe_allow_html=True)
+    st.write() #This is to have gap between
+    #summ_gpt = st.button("Summarize")
+    if session_state.s1 = False:
+        if st.button('Summarize'):
+            with st.spinner("Summarizing...."):
+        
+                st.write("this is the summarization")
+    
+                st.markdown("#### Summarization Feedback:")
+                col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
+    
+                if session_state.s1 = True:
+                    with col_1:
+                        if st.button("👍🏻",key=4):
+                            
+                            session_state.s2 = True
+                            
+    
+    
+                    with col_2:
+                        if st.button("👎🏻",key=5):
+                            
+    
+    
+                            session_state.s2 = True
+                           
 
-# Create the second button
-if st.button('Button 2'):
-    # Set the session state variable to True when Button 2 is clicked
-    session_state.button_clicked = True
-
-# Show a message if Button 2 has been clicked
-if session_state.button_clicked:
-    st.write('Button 2  was ##clicked')
+    if session_state.s2 == True:
+       st.write('Feedback is recorded!')
