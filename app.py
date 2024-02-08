bcc84434de4d93287cde7e3086fcac84f92b808b
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 22:14:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def nested_button_function():
     # First button
-    if st.button("Click me to show the second button"):
+    if st.button("button1"):
+        st.write("first button clicked!")
         # Display the second button only if the first button is clicked
         if st.session_state.show_second_button is None:
             st.session_state.show_second_button = False
@@ -11,7 +12,7 @@ def nested_button_function():
     # Second button
     if st.session_state.show_second_button:
         # This button won't reload the page
-        if st.button("Second Button (No Page Reload)"):
+        if st.button("button 2"):
             st.write("Second button clicked!")
 
 # Initialize session state
