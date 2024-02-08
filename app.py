bb26ac37e5447c991aed6d74ef8db25de9edd4a2
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 13:11:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,12 @@
-import pandas as pd 
 import streamlit as st
-def summarize_gpt():
 
-    
-    if 'summary_genenrated' not in st.session_state:
-        st.session_state.summary_genenrated = True
+# Create a placeholder to dynamically update content
+output_placeholder = st.empty()
 
+# Create a button
+button_clicked = st.button("Click me!")
 
-    st.markdown("""<span style="font-size: 24px; ">Summarize key findings of the case.</span>""", unsafe_allow_html=True)
-    st.write() #This is to have gap between
-    summ_gpt = st.button("Summarize-me")
-    if st.session_state.summary_genenrated == True:
-        
-        if summ_gpt:
-            
-            with st.spinner("Summarizing...."):
-                st.session_state.summary_genenrated = True
-                st.write("abcdefgh")
-                st.write("1234")
-                
-                st.markdown("#### Summarization Feedback:")
-                col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
-    
-                with col_1:
-                        z=st.button("👍🏻",key=4)
-                        if z:
-                            st.write("*Feedback is recorded*")
-                            st.session_state.summary_genenrated = False
-                            
-                            
-                         
-        elif  st.session_state.summary_genenrated == False:
-                                st.write("hurray")                 
-    
-
-
-
-
-
-summarize_gpt()
-
-
-                   
-
-
-
+# Check if the button is clicked
+if button_clicked:
+    # Update the content of the placeholder without reloading the entire page
+    output_placeholder.write("Button clicked!")
