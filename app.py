f1f960c9ee64d4dddab05ce7261a952a6239b021
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-08 12:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,11 @@ def summarize_gpt():
     
     if 'summary_genenrated' not in st.session_state:
         st.session_state.summary_genenrated = True
-    
-    # def set_clicked2():
-    #     st.session_state.clicked2 = True
-    #     st.session_state.disabled = True
+
 
     st.markdown("""<span style="font-size: 24px; ">Summarize key findings of the case.</span>""", unsafe_allow_html=True)
     st.write() #This is to have gap between
-    summ_gpt = st.button("Summarize")
+    summ_gpt = st.button("Summarize-me")
     if summ_gpt:
         with st.spinner("Summarizing...."):
             st.session_state.summary_genenrated = True
@@ -23,11 +20,9 @@ def summarize_gpt():
             col_1, col_2, col_3, col_4, col_5, col_6 = st.columns(6)
 
             with col_1:
-                if st.button("👍🏻",key=4):
-                    
-                    st.session_state.summary_genenrated = False
+                if st.button("👍🏻",key=4)
                     st.write("*Feedback is recorded*")
-                    # st.session_state.summary_genenrated = True
+                    st.session_state.summary_genenrated = True
 
 
             with col_2:
@@ -38,7 +33,7 @@ def summarize_gpt():
                     st.write("*Feedback is recorded*")
                     # st.session_state.summary_genenrated = True
 
-    elif st.session_state.summary_genenrated == False:
+    elif st.session_state.summary_genenrated == True:
             st.write("*Thank you, your feedback is recorded*") 
 
 
