6fdc76ba23d5e0ead943092a69701a3a2841349d
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-15 17:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-# Set the font size
-font_size = 16
-
-# Create a text area with specified font size
-text = st.text_area("Enter text here", height=200, key="text_area", value="", help="Text area", font_size=font_size)
+label = r'''
+$\textsf{
+    \Huge Text \huge Text \LARGE Text \Large Text 
+    \large Text \normalsize Text \small Text 
+    \footnotesize Text \scriptsize Text \tiny Text 
+}$
+'''
