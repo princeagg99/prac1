771ca483bb47d306b79fc81616c0017720b4cb52
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-15 17:31:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.session_state.user_summary = "abcd"
 #st.text_area("**Update the system generated summary**", st.session_state.user_summary, height = 300, help = "Once the system summarizes the key findings of the case, you can edit it as per your consideration. After downloading the report, the user summary is reset.", placeholder = "Press Summarize button above to summarize the key findings of the case.")
 
-st.text_area(r"$\textsf{\Tiny  Enter text here}$")
+st.text_area("**Update the system generated summary**", st.session_state.user_summary, height = 300, help = "Once the system summarizes the key findings of the case, you can edit it as per your consideration. After downloading the report, the user summary is reset.", placeholder = "Press Summarize button above to summarize the key findings of the case.")
