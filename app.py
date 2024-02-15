17466eb7db0703651c3a211b9ae6ffef93a27091
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-15 17:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,2 @@
 import streamlit as st
-st.text_area("Write some text")
-st.markdown(
-    """
-    <style>
-    textarea {
-        font-size: 3rem !important;
-    }
-    input {
-        font-size: 3rem !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+st.text_area("Write some text",font_size=40)
