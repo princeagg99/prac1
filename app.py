f2e23e107360be6cc9ba006b7a120cf4adf57c34
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-26 14:21:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ with col1:
 
 with col2:
     button2 = st.button('Button 2')
+    if button2:
+        st.write("hello2")
+        # Do something...
 
 with col3:
     button3 = st.button('Button 3')
 
 
 
-if button2:
-    st.write("hello2")
-    # Do something...
+
 
 if button3:
     st.write("hello3")
