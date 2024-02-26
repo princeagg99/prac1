4df428f1693a0079e13e7e7ae73aa47507ae354c
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-26 14:20:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     button1 = st.button('Button 1')
     if button1:
-    st.write("hello1")
+        st.write("hello1")
 
 with col2:
     button2 = st.button('Button 2')
