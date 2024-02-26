f5e922cc86725a38167e6bcd0ed0ac90082fe08e
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-02-26 14:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
-  
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    button1 = st.button('Button 1')
+
+with col2:
+    button2 = st.button('Button 2')
+
+with col3:
+    button3 = st.button('Button 3')
+
+if button1:
+    st.write("hello1")
+
+if button2:
+    st.write("hello2")
+    # Do something...
+
+if button3:
+    st.write("hello3")
+    # Do something...
